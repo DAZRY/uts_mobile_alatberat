9ad5dcbd32ce0bf638897df0b680497735055fa8
Lendra <lendradzr77@gmail.com>
2026-04-26 20:06:46 +0700

    catalog repository

---
+++ lib/features/catalog/domain/data/catalog_repository.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:uts_mobile_alatberat/core/constants/api_constants.dart';
+import 'package:uts_mobile_alatberat/features/catalog/domain/product_model.dart';
+
+class CatalogRepository {
+  Future<List<ProductModel>> getProducts(String jwt) async {
+    final response = await http.get(
+      Uri.parse(ApiConstants.products),
+      headers: {'Authorization': 'Bearer $jwt'},
+    );
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      final List data = body['data'];
+      return data.map((e) => ProductModel.fromJson(e)).toList();
+    } else {
+      throw Exception('Gagal mengambil produk');
+    }
+  }
+}
